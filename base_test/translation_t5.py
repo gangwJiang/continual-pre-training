59e7fda1742093d998f0df0ac1ca081377837aee
gwjiang <gwjiang@mail.ustc.edu.cn>
2022-06-21 13:39:42 +0800

    basic test

--- base_test/translation_t5.py
+++ base_test/translation_t5.py
@@ -1,6 +1,7 @@
 from datasets import load_dataset
 from transformers import DataCollatorForSeq2Seq
 from transformers import AutoTokenizer
+from transformers import AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2SeqTrainer
 
 def preprocess_function(examples):
     inputs = [prefix + example[source_lang] for example in examples["translation"]]
@@ -16,7 +17,9 @@ def preprocess_function(examples):
 books = load_dataset("opus_books", "en-fr")
 books = books["train"].train_test_split(test_size=0.2)
 
-tokenizer = AutoTokenizer.from_pretrained("t5-small")
+model_name = "t5-base"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 source_lang = "en"
 target_lang = "fr"
@@ -24,4 +27,28 @@ prefix = "translate English to French: "
 
 tokenized_books = books.map(preprocess_function, batched=True)
 
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
+
+training_args = Seq2SeqTrainingArguments(
+    output_dir="../results",
+    evaluation_strategy="epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    weight_decay=0.01,
+    save_total_limit=3,
+    num_train_epochs=1,
+    fp16=True,
+)
+
+trainer = Seq2SeqTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_books["train"],
+    eval_dataset=tokenized_books["test"],
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+)
+
+trainer.train()
